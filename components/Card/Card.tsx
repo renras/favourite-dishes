8d80a72c5af5392afc5577bb0d585f3c467a3f7a
renras <85605271+renras@users.noreply.github.com>
2022-03-02 14:05:20 +0800

    Revert "animate text content of cards in food list"

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,7 +1,5 @@
 import Image from "next/image";
 
-import styles from "./Card.module.css";
-
 interface Props {
   name: string;
   image: string;
@@ -13,11 +11,9 @@ const Card = ({ name, image, description, rating }: Props) => {
   return (
     <div>
       <Image src={image} alt={name} width={200} height={200} />
-      <h2 className={styles.title}>{name}</h2>
-      <div className={styles.text}>
-        <p>{description}</p>
-        <p>Rating: {rating}</p>
-      </div>
+      <h2>{name}</h2>
+      <p>{description}</p>
+      <p>Rating: {rating}</p>
     </div>
   );
 };
