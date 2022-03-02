ab3159d86695005de0314aa1d500fe2acc925344
Renzo Visperas <renzovisperas07@gmail.com>
2022-03-03 07:03:09 +0800

    unseparate logic from index page, remove lib and context folders

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,58 @@
+import { useState } from "react";
 import type { NextPage } from "next";
 import { GetStaticProps, InferGetStaticPropsType } from "next";
 import Head from "next/head";
 
 import Darkmode from "darkmode-js";
-import useFetch from "../lib/Pages/HomeLogic";
 import favouriteDishes from "../dishes-favourite";
 import Search from "../components/Search/Search";
 import Filter from "../components/Filter/Filter";
 import Dishes from "../components/Dishes/Dishes";
 
+interface Dish {
+  name: string;
+  image: string;
+  description: string;
+  rating: number;
+  id: number;
+  placeholder: string;
+}
+
 const Home: NextPage = ({
   favouriteDishes,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  const { inputHandleChange, filterHandleChange, dishes, text } = useFetch({
-    favouriteDishes,
-  });
+  const [dishes, setDishes] = useState<Dish[]>(favouriteDishes);
+  const [text, setText] = useState("");
+
+  const inputHandleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+
+    if (e.target.value === "") {
+      setDishes(favouriteDishes);
+      return;
+    }
+
+    const newDishes = dishes.filter((dish: Dish) =>
+      dish.name.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+    setDishes(newDishes);
+  };
+
+  const filterHandleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    if (e.target.value === "Ascending") {
+      const newDish = favouriteDishes.sort(
+        (a: Dish, b: Dish) => a.rating - b.rating
+      );
+      setDishes([...newDish]);
+    }
+
+    if (e.target.value === "Descending") {
+      const newDish = favouriteDishes.sort(
+        (a: Dish, b: Dish) => b.rating - a.rating
+      );
+      setDishes([...newDish]);
+    }
+  };
 
   new Darkmode().showWidget();
 
