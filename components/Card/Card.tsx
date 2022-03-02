d039f4172fdc3fe17479fb7f7d04eeea0c73eac1
renras <85605271+renras@users.noreply.github.com>
2022-03-02 14:02:58 +0800

    Revert "add button to view image, reveal image onclick, animate image"

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
-
 import Image from "next/image";
+
 import styles from "./Card.module.css";
 
 interface Props {
@@ -11,28 +10,9 @@ interface Props {
 }
 
 const Card = ({ name, image, description, rating }: Props) => {
-  const [isImageHidden, setIsImageHidden] = useState(true);
-
-  const imgClickHandler = () => {
-    setIsImageHidden(!isImageHidden);
-  };
-
   return (
     <div>
-      <div
-        className={`${styles.imageWrapper_1} ${
-          !isImageHidden && styles.hidden
-        }`}
-        onClick={imgClickHandler}
-      >
-        <button>NSFW Click to view image</button>
-      </div>
-      <div
-        className={`${styles.imageWrapper_2} ${isImageHidden && styles.hidden}`}
-        onClick={imgClickHandler}
-      >
-        <Image src={image} alt={name} width={200} height={200} />
-      </div>
+      <Image src={image} alt={name} width={200} height={200} />
       <h2 className={styles.title}>{name}</h2>
       <div className={styles.text}>
         <p>{description}</p>
