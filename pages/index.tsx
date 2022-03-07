d092b2fac6c71a6f5a11157ce65292d11ef0c100
Renzo Visperas <renzovisperas07@gmail.com>
2022-03-07 09:31:13 +0800

    add submit handler as a prop to the form

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { useContext, useEffect } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import AppContext from "../context/AppContext";
+import { SubmitHandler } from "react-hook-form";
 
 import Darkmode from "darkmode-js";
 import Dishes from "../components/Dishes/Dishes";
@@ -9,6 +10,13 @@ import Modal from "../components/Modal/Modal";
 import Form from "../components/Form/Form";
 import GoBackButton from "../components/GoBackButton/GoBackButton";
 import Options from "../components/Options/Options";
+
+type IFormInput = {
+  title: string;
+  imgUrl: string;
+  description: string;
+  rating: number;
+};
 
 const Home: NextPage = () => {
   const { state, dispatch } = useContext(AppContext);
@@ -20,6 +28,24 @@ const Home: NextPage = () => {
 
   const toggleModal = () => {
     dispatch({ type: "TOGGLE_MODAL", payload: !state.showModal });
+    document.documentElement.style.setProperty("--overflow", "auto");
+  };
+
+  const formSubmitHandler: SubmitHandler<IFormInput> = (data) => {
+    dispatch({
+      type: "ADD_DISH",
+      payload: {
+        id: state.dishes.length + 1,
+        name: data.title,
+        image: data.imgUrl,
+        description: data.description,
+        rating: data.rating,
+      },
+    });
+    dispatch({ type: "SET_INPUT_TEXT", payload: "" });
+    dispatch({ type: "FILTER_DISHES" });
+    dispatch({ type: "TOGGLE_MODAL", payload: false });
+
     document.documentElement.style.setProperty("--overflow", "auto");
   };
 
@@ -37,7 +63,7 @@ const Home: NextPage = () => {
       {state.showModal && (
         <Modal>
           <GoBackButton clickHandler={toggleModal} />
-          <Form />
+          <Form formSubmitHandler={formSubmitHandler} />
         </Modal>
       )}
     </>
