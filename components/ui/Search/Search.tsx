f903a9776e0ee26a5068103a5eafb5eb22010500
Renzo Visperas <renzovisperas07@gmail.com>
2022-03-29 13:12:25 +0800

    add styling to search bar using mui search

--- components/ui/Search/Search.tsx
+++ components/ui/Search/Search.tsx
@@ -1,9 +1,29 @@
+import SearchIcon from "@mui/icons-material/Search";
+import InputAdornment from "@mui/material/InputAdornment";
+
+import TextField from "@mui/material/TextField";
+
 interface Props {
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 const Search = ({ onChange }: Props) => {
-  return <input type="text" placeholder="Search.." onChange={onChange} />;
+  return (
+    <TextField
+      id="input-with-icon-textfield"
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <SearchIcon />
+          </InputAdornment>
+        ),
+      }}
+      onChange={onChange}
+      placeholder="Search"
+      variant="outlined"
+      size="small"
+    />
+  );
 };
 
 export default Search;
