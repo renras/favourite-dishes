a890ea4900df1fdab0160b7f0affb83f9f580c7d
Renzo Visperas <renzovisperas07@gmail.com>
2022-03-30 14:41:39 +0800

    add log-in button

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,17 +1,25 @@
 import AppBar from "@mui/material/AppBar";
-import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <AppBar position="static">
-        <Container maxWidth="xl" disableGutters sx={{ padding: "10px 0" }}>
-          <Typography variant="h4" component="h1">
-            MYFAVORITES
-          </Typography>
-        </Container>
-      </AppBar>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Container maxWidth="xl" disableGutters>
+            <Toolbar>
+              <Typography variant="h4" component="h1" sx={{ flexGrow: 1 }}>
+                MYFAVORITES
+              </Typography>
+              <Button color="inherit">Login</Button>
+            </Toolbar>
+          </Container>
+        </AppBar>
+      </Box>
       <main style={{ paddingBottom: "50px" }}>{children}</main>
     </>
   );
