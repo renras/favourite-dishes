b5ae047ce5a76a8bf950b511c0af51298a65c110
renras <renzovisperas07@gmail.com>
2022-06-06 11:03:47 +0800

    refractor _app to use prisma session

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,20 @@
 import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import { AppContextProvider } from "../context/AppContext";
 
 import Layout from "../components/Layout/Layout";
 import "../styles/globals.css";
-import { AppContextProvider } from "../context/AppContext";
 
-function MyApp({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
   return (
+    <SessionProvider session={pageProps.session}>
       <AppContextProvider>
         <Layout>
           <Component {...pageProps} />
         </Layout>
       </AppContextProvider>
+    </SessionProvider>
   );
-}
+};
 
-export default MyApp;
+export default App;
