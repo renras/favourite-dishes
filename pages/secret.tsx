9c000e486f5eaa9305d0cd6aca342ae1d5cc4c50
renras <renzovisperas07@gmail.com>
2022-06-06 14:31:26 +0800

    initialize protected page

---
+++ pages/secret.tsx
@@ -0,0 +1,22 @@
+import { useSession } from "next-auth/react";
+
+const Secret = () => {
+  const { status } = useSession();
+
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
+
+  if (status === "unauthenticated") {
+    return <p>Access Denied</p>;
+  }
+
+  return (
+    <>
+      <h1>Protected Page</h1>
+      <p>You can view this page because you are signed in.</p>
+    </>
+  );
+};
+
+export default Secret;
