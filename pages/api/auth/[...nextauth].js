62774239d8601d54c90aaf43324f8ce567d10638
renras <renzovisperas07@gmail.com>
2022-06-07 21:48:32 +0800

    handle error in credentials provider authorization

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -13,25 +13,24 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
       name: "Credentials",
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
         username: { label: "Username", type: "text" },
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
-        const res = await axios.get(
-          `http://localhost:3000/api/v1/user?username=${credentials.username}`
-        );
-        const user = res.data.data;
+        try {
+          const res = await axios.get(
+            `http://localhost:3000/api/v1/user?username=${credentials.username}`
+          );
+          const user = res.data.data;
 
-        if (user) return user;
+          if (user) return user;
 
-        return null;
+          return null;
+        } catch (error) {
+          throw new Error("Internal Server Error.");
+        }
       },
     }),
   ],
