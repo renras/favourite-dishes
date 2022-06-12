8e063255d9cb6ddd62a93f6ecba80813df2c3f03
renras <renzovisperas07@gmail.com>
2022-06-12 22:36:08 +0800

    use error and helpertext prop in mui textfield

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -1,11 +1,9 @@
 import { Dispatch, SetStateAction } from "react";
 import { useForm, SubmitHandler, Controller } from "react-hook-form";
 import isURL from "validator/lib/isURL";
-import { isValidPhoneNumber } from "libphonenumber-js";
 import TextField from "@mui/material/TextField";
 import Container from "@mui/material/Container";
 import Paper from "@mui/material/Paper";
-import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import CloseIcon from "@mui/icons-material/Close";
 import Box from "@mui/material/Box";
@@ -63,18 +61,19 @@ const Form = ({ onClose }: { onClose: Dispatch<SetStateAction<boolean>> }) => {
           control={control}
           rules={{ required: true }}
           render={({ field }) => (
-            <TextField {...field} label="Title" variant="outlined" fullWidth />
+            <TextField
+              {...field}
+              label="Title"
+              variant="outlined"
+              fullWidth
+              error={!!errors.title}
+              helperText={
+                (errors.title?.type === "required" && "Title is required.") ||
+                ""
+              }
+            />
           )}
         />
-        {errors.title?.type === "required" && (
-          <Typography
-            variant="caption"
-            component="p"
-            sx={{ marginLeft: "5px", color: "#e57373" }}
-          >
-            Title is required.
-          </Typography>
-        )}
         <Controller
           name="imgUrl"
           control={control}
@@ -86,27 +85,17 @@ const Form = ({ onClose }: { onClose: Dispatch<SetStateAction<boolean>> }) => {
               variant="outlined"
               fullWidth
               sx={{ marginTop: "25px" }}
+              error={!!errors.imgUrl}
+              helperText={
+                (errors.imgUrl?.type === "required" &&
+                  "Image src is required.") ||
+                (errors.imgUrl?.type === "validate" &&
+                  "Please enter a valid url.") ||
+                ""
+              }
             />
           )}
         />
-        {errors.imgUrl?.type === "required" && (
-          <Typography
-            variant="caption"
-            component="p"
-            sx={{ marginLeft: "5px", color: "#e57373" }}
-          >
-            Title is required.
-          </Typography>
-        )}
-        {errors.imgUrl?.type === "validate" && (
-          <Typography
-            variant="caption"
-            component="p"
-            sx={{ marginLeft: "5px", color: "#e57373" }}
-          >
-            Title is required.
-          </Typography>
-        )}
         <Controller
           name="description"
           control={control}
@@ -118,18 +107,15 @@ const Form = ({ onClose }: { onClose: Dispatch<SetStateAction<boolean>> }) => {
               variant="outlined"
               fullWidth
               sx={{ marginTop: "25px" }}
+              error={!!errors.description}
+              helperText={
+                (errors.description?.type === "required" &&
+                  "Description is required") ||
+                ""
+              }
             />
           )}
         />
-        {errors.description?.type === "required" && (
-          <Typography
-            variant="caption"
-            component="p"
-            sx={{ marginLeft: "5px", color: "#e57373" }}
-          >
-            Description is required.
-          </Typography>
-        )}
         <Controller
           name="rating"
           control={control}
@@ -142,57 +128,17 @@ const Form = ({ onClose }: { onClose: Dispatch<SetStateAction<boolean>> }) => {
               variant="outlined"
               fullWidth
               sx={{ marginTop: "25px" }}
+              error={!!errors.rating}
+              helperText={
+                (errors.rating?.type === "required" && "Rating is required") ||
+                ((errors.rating?.type === "min" ||
+                  errors.rating?.type === "max") &&
+                  "Please pick a number from 1 to 5") ||
+                ""
+              }
             />
           )}
         />
-        {errors.rating?.type === "required" && (
-          <Typography
-            variant="caption"
-            component="p"
-            sx={{ marginLeft: "5px", color: "#e57373" }}
-          >
-            Rating is required.
-          </Typography>
-        )}
-        {errors.rating?.type === ("min" || "max") && (
-          <Typography
-            variant="caption"
-            component="p"
-            sx={{ marginLeft: "5px", color: "#e57373" }}
-          >
-            Pick a number from 1 to 5.
-          </Typography>
-        )}
-        <Controller
-          name="phone"
-          control={control}
-          rules={{
-            required: false,
-            validate: {
-              isValidPhoneNumber: (value) =>
-                value !== "" ? isValidPhoneNumber(value as string, "PH") : true,
-            },
-          }}
-          render={({ field }) => (
-            <TextField
-              type="tel"
-              {...field}
-              label="Phone"
-              variant="outlined"
-              fullWidth
-              sx={{ marginTop: "25px" }}
-            />
-          )}
-        />
-        {errors.phone?.type === "isValidPhoneNumber" && (
-          <Typography
-            variant="caption"
-            component="p"
-            sx={{ marginLeft: "5px", color: "#e57373" }}
-          >
-            Enter a valid phone number.
-          </Typography>
-        )}
         <Box sx={{ display: "flex", justifyContent: "center", gap: "1rem" }}>
           <Button
             type="submit"
